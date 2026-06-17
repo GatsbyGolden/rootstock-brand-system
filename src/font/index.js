ffb92efc635a8f772b76d65536dbfdbf8a03b15f
Sam Golden <samuel.golden@rootstocklabs.com>
2026-06-17 12:22:04 +0300

    Remove npm package files

--- src/font/index.js
+++
@@ -1,3 +0,0 @@
-export { RootstockSansHeadline, default as RootstockSansHeadlineFont } from './headline/index.js';
-export { RootstockSansBody, default as RootstockSansBodyFont } from './body/index.js';
-export { RootstockSansTags, default as RootstockSansTagsFont } from './tags/index.js';
--- src/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { NextFont } from 'next/dist/compiled/@next/font';
-
-export declare const RootstockSansHeadline: NextFont;
-export declare const RootstockSansBody: NextFont;
-export declare const RootstockSansTags: NextFont;
